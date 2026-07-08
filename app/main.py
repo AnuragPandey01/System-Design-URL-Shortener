02bbf775f9494bd91607c290e66635a86873dd08
Anurag Pandey <anuragpandey87187@gmail.com>
2026-07-08 18:07:38 +0530

    add redis support

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from sqlmodel import Session, select
 from starlette import status
 from starlette.responses import RedirectResponse
 import uvicorn
+import redis
 from core.zookeeper import ZooKeeperTokenManager
 from db.session import get_db
 from models.url_models import URLs
@@ -16,6 +17,7 @@ import base62
 app = FastAPI()
 
 zk_manager = ZooKeeperTokenManager(zk_hosts="127.0.0.1:2181")
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 @app.get("/")
 def read_root():
@@ -50,11 +52,18 @@ def shorten_url(req: ShortenRequest, db: Annotated[Session, Depends(get_db)]):
 @app.get("/api/v1/{short_code}")
 def redirect_url(short_code: str, db: Annotated[Session, Depends(get_db)]):
 
+    cached_url = redis_client.get(short_code)
+
+    if cached_url:
+        return RedirectResponse(url=cached_url, status_code=302)
+
     statement = select(URLs).where(URLs.short_code == short_code)
     entry = db.exec(statement).first()
 
     if not entry:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    redis_client.set(short_code, entry.long_text, ex=3600)
 
     return RedirectResponse(url=entry.long_text, status_code=status.HTTP_302_FOUND)
 
